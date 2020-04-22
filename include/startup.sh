de14b41f08f480c0cc7671b1062dd2d1b25e8708
BenLue <benlue@s3root.ovh>
2020-04-22 17:15:14 +0200

    include/startupsh: remove ANDROID_JACK_VM_ARGS

--- include/startup.sh
+++ include/startup.sh
@@ -15,7 +15,7 @@ fi
 
 export USE_CCACHE=1
 export CCACHE_COMPRESS=1
-export ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4G"
+
 repo init -u https://github.com/LineageOS/android.git -b lineage-17.1
 
 # Show general info
